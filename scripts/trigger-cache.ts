1472613e5d8b2969e17ccc1b86e214015be12847
jaexochill2-code <jaexochill2@gmail.com>
2026-03-11 20:18:32 +0800

    fix-remove-broken-trigger-cache-script

--- scripts/trigger-cache.ts
+++
@@ -1,9 +0,0 @@
-import { updateContextCache } from '../src/lib/gemini-cache-manager';
-
-async function run() {
-  console.log("Triggering cache creation manually...");
-  await updateContextCache();
-  console.log("Done.");
-}
-
-run();
